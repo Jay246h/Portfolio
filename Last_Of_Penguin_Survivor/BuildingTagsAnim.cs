2117ab140b7b4c4fe7d918c855ee2928df628318
Jay246h <131239052+Jay246h@users.noreply.github.com>
2025-05-20 01:09:50 +0900

    Create BuildingTagsAnim.cs

---
+++ Last_Of_Penguin_Survivor/BuildingTagsAnim.cs
@@ -0,0 +1,7 @@
+public class BuildingTagsAnim
+{
+    public const string Player = nameof(Player);    // 나무가 감지하는 플레이어 태그
+    public const string isHit = nameof(isHit);      // 나무가 맞는 애니메이션
+    public const string FruitTree = nameof(FruitTree);  // 열매 나무
+    public const string Tree = nameof(Tree);            // 그냥 나무
+}
