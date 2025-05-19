d38cb286ea4172d47159bc6494d572a124960ba7
Jay246h <131239052+Jay246h@users.noreply.github.com>
2025-05-20 01:17:35 +0900

    Create ItemPickUp.cs

---
+++ GameMaGoGameJam/ItemPickUp.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public Item Item;
+
+    void PickUp()
+    {
+        InventoryManager.Instance.Add(Item);
+        Destroy(gameObject);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Drill"))
+        {
+            PickUp();
+        }
+    }
+}
