b0349697f5e3e82bf68e464ed17ebacce9324722
Jay246h <131239052+Jay246h@users.noreply.github.com>
2025-05-20 01:19:26 +0900

    Create GameController.cs

---
+++ Project-G/GameController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private UIController uiController;
+    [SerializeField]
+    //private	GameObject		pattern01;
+
+    // 플레이어 점수 (죽지않고 버틴 시간)
+    public float CurrentScore { private set; get; } = 0;
+
+    public bool IsGamePlay { private set; get; } = false;
+
+    public void GameStart()
+    {
+
+    }
+
+    public void GameExit()
+    {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void GameOver()
+    {
+        uiController.GameOver();
+    }
+}
